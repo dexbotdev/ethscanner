c1259f4da27bcb4b65d50f1c0608fc69a10ba665
DefiCody <ambergridpro@gmail.com>
2023-06-05 20:45:23 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,23 +1,309 @@
-import logo from './logo.svg';
 import './App.css';
+import { Stack, TextField, Button } from '@mui/material';
+import { useRef, useState } from 'react';
+import { HoneypotIsV1 } from '@normalizex/honeypot-is';
+import Backdrop from '@mui/material/Backdrop';
+import CircularProgress from '@mui/material/CircularProgress';
 
 function App() {
+
+  const valueRef = useRef('')
+  const [open, setOpen] = useState(false);
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const [tokenScanData, setTokenScanData] = useState(undefined);
+  const sendValue = async () => {
+    handleOpen();
+    const CHAIN_ID = 1;
+    const honeypotis = new HoneypotIsV1();
+
+    const BUSD = valueRef.current.value;
+
+    if (BUSD !== undefined && BUSD !== "") {
+      const BUSD_PAIRS = await honeypotis.getPairs(BUSD, CHAIN_ID);
+
+      await honeypotis.honeypotScan(
+        BUSD,
+        BUSD_PAIRS[0].Router,
+        BUSD_PAIRS[0].Pair,
+        CHAIN_ID
+      ).then((result) => {
+        console.log(result)
+        setTokenScanData(result)
+        handleClose();
+      }).catch(Error => {
+        handleClose();
+      })
+    } else {
+
+    }
+
+
+    //on clicking button accesing current value of TextField and outputing it to console 
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <div style={{ 'marginTop': '60px', 'textAlign': 'center' }}>
+        <TextField
+          label="Input Token Address"
+          inputRef={valueRef}
+          placeholder="0x......."
+          sx={{ 'backgroundColor': '#fdfdff', 'width': '50%', 'borderRadius': '20px' }}
+          InputProps={{
+            endAdornment: (
+              <Stack direction="row" spacing={1}>
+                <Button sx={{ 'color': '#02feff' }} variant="contained" color="primary"
+                  onClick={sendValue}>Search</Button>
+              </Stack>
+            ),
+          }}
+        />
+
+      </div>
+      <Backdrop
+        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={open}
+        onClick={handleClose}
+      >
+        <CircularProgress color="inherit" />
+      </Backdrop>
+      {tokenScanData && <div>
+        <div id="leftmenu">
+
+          <div id="date_time">
+            <div id="date" className="semi_arc e4">
+              <div className="semi_arc_2 e4_1">
+                <div className="counterspin4"></div>
+              </div>
+              <div style={{ 'fontSize': '40px', 'marginTop': '25px' }}>{tokenScanData.SellTax}</div>
+              <div style={{ 'fontSize': '25px' }}>Sell Tax</div>
+            </div>
+
+            <div id="time" className="arc e1">
+              <div style={{ 'fontSize': '23px', 'marginLeft': '-10px', 'marginTop': '23px' }}>{tokenScanData.BuyTax}</div>
+               <div style={{ 'fontSize': '17px', 'marginTop': '10px' }}>Buy Tax</div>
+            </div>
+          </div>
+
+          <p className="title">Token Info</p>
+          <div className="hline title_underline"></div>
+
+          <span className="menuitem entypo-gauge" style={{ 'fontSize': '30px', 'marginLeft': '10px' }}>
+            <p id="cpu" className="caption" style={{ 'fontSize': '20px', 'marginLeft': '10px' }}>Name: {tokenScanData.Token.Name}</p>
+          </span> <br />
+          <span className="menuitem entypo-gauge" style={{ 'fontSize': '30px', 'marginLeft': '10px' }}>
+            <p id="cpu" className="caption" style={{ 'fontSize': '20px', 'marginLeft': '10px' }}>Symbol: {tokenScanData.Token.Symbol}</p>
+          </span> <br />
+          <span className="menuitem entypo-gauge" style={{ 'fontSize': '30px', 'marginLeft': '10px' }}>
+            <p id="cpu" className="caption" style={{ 'fontSize': '20px', 'marginLeft': '10px' }}>Decimals: {tokenScanData.Token.Decimals}</p>
+          </span> <br />
+          <span className="menuitem entypo-gauge" style={{ 'fontSize': '30px', 'marginLeft': '10px' }}>
+            <p id="cpu" className="caption" style={{ 'fontSize': '20px', 'marginLeft': '10px' }}>Network: ETHEREUM</p>
+          </span> <br />
+
+
+          <p className="title">HoneyPot?</p>
+          <div className="hline title_underline"></div>
+
+          <div className="menu">
+            <button className="menuitem"> <span className="entypo-right-open" />
+              <p className="caption">{!tokenScanData.IsHoneypot?'PASSED':'FAILED'}</p>
+            </button> 
+          </div>
+
+        </div>
+        <div id="rightmenu"> 
+
+        <p className="title">Price Info</p>
+          <div className="hline title_underline"></div> 
+ 
+
+          <span className="menuitem entypo-chart-area" style={{ 'fontSize': '30px', 'marginLeft': '10px' }}>
+            <p id="ram" className="caption" style={{ 'fontSize': '20px', 'marginLeft': '10px' }}>Liquidity: {tokenScanData.Pair.Liquidity}</p>
+          </span> <br />
+          <span className="menuitem entypo-chart-area" style={{ 'fontSize': '30px', 'marginLeft': '10px' }}>
+            <p id="ram" className="caption" style={{ 'fontSize': '20px', 'marginLeft': '10px' }}>MarketCap: {tokenScanData.Pair.Liquidity}</p>
+          </span> <br />
+ 
+          <p className="title">Network Info</p>
+          <div className="hline title_underline"></div> 
+          <div id="date_time">
+            <div id="date" className="semi_arc e4">
+              <div className="semi_arc_2 e4_1">
+                <div className="counterspin4"></div>
+              </div>
+              <div style={{ 'fontSize': '40px', 'marginTop': '25px' }}>{tokenScanData.BuyGas}</div>
+              <div style={{ 'fontSize': '25px' }}>Buy Gas</div>
+            </div>
+
+            <div id="time" className="arc e1">
+              <div style={{ 'fontSize': '23px', 'marginLeft': '-10px', 'marginTop': '23px' }}>{tokenScanData.SellGas}</div>
+               <div style={{ 'fontSize': '17px', 'marginTop': '10px' }}>Sell Gas</div>
+            </div>
+          </div>
+        </div>
+        <div id="arc_container">
+          <div className="arc_reactor">
+            <div className="case_container">
+              <div className="e7">
+                <div className="semi_arc_3 e5_1">
+                  <div className="semi_arc_3 e5_2">
+                    <div className="semi_arc_3 e5_3">
+                      <div className="semi_arc_3 e5_4"></div>
+                    </div>
+                  </div>
+                </div>
+                <div className="core2"></div>
+              </div>
+              <ul className="marks">
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <canvas id="particle1" width="20" height="500"></canvas>
+        <script>
+          var canvas = document.getElementById('particle1');
+          var context = canvas.getContext('2d');
+          context.beginPath();
+          context.moveTo(0, 0);
+          context.lineTo(0, 70);
+          context.lineTo(10, 85);
+          context.lineTo(10, 135);
+          context.lineTo(0, 150);
+          context.lineTo(0, 480);
+          context.lineTo(5, 490);
+          context.lineTo(10, 490);
+          context.lineTo(20, 490);
+          context.lineTo(20, 250);
+          context.lineTo(10, 235);
+          context.lineTo(10, 185);
+          context.lineTo(20, 170);
+          context.lineTo(20, 40);
+          context.lineTo(10, 30);
+          context.lineTo(10, 20);
+          context.closePath();
+          context.lineWidth = 1;
+          context.fillStyle = 'rgba(2,254,255,0.3)';
+          context.fill();
+          context.strokeStyle = 'transparent';
+          context.stroke();
+        </script>
+
+        <canvas id="particle1_1" width="40" height="510"></canvas>
+        <script>
+          var canvas = document.getElementById('particle1_1');
+          var context = canvas.getContext('2d');
+          context.beginPath();
+          context.lineTo(0, 0);
+          context.lineTo(10, 15);
+          context.lineTo(10, 65);
+          context.lineTo(0, 80);
+          context.lineTo(0, 0);
+          context.closePath();
+          context.lineWidth = 1;
+          context.fillStyle = 'rgba(2,254,255,0.3)';
+          context.fill();
+          context.strokeStyle = 'transparent';
+          context.stroke();
+        </script>
+
+        <canvas id="particle1_2" width="40" height="510"></canvas>
+        <script>
+          var canvas = document.getElementById('particle1_2');
+          var context = canvas.getContext('2d');
+          context.beginPath();
+          context.lineTo(10, 80);
+          context.lineTo(0, 65);
+          context.lineTo(0, 15);
+          context.lineTo(10, 0);
+          context.lineTo(10, 80);
+          context.closePath();
+          context.lineWidth = 1;
+          context.fillStyle = 'rgba(2,254,255,0.3)';
+          context.fill();
+          context.strokeStyle = 'transparent';
+          context.stroke();
+        </script>
+
+        <div id="particle1_3">
+  > > > >
+        </div>
+
+        <div id="particle2">▶<br />▶<br />▶</div>
+        <div id="particle3" className="vline" />
+        <div id="particle4" className="vline" />
+        <div id="particle5" className="vline" />
+        <div id="particle6" className="vline" />
+        <div id="particle7" className="vline" />
+        <div id="particle8" className="vline" />
+        <div id="particle9" className="vline" />
+      </div>}
     </div>
   );
 }
